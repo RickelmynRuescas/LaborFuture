b6f3e4cc93bdd0b4a6e5d97381664944a7700e28
Rickelmyn Ruescas <rickelmyndesouzaruescas@gmail.com>
2025-11-19 12:47:03 -0300

    Enhance UsuarioDTO with additional fields
    Added new fields for pet name, birthplace, and childhood nickname with validation.

--- src/main/java/br/com/fiap/model/dto/UsuarioDTO.java
+++ src/main/java/br/com/fiap/model/dto/UsuarioDTO.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UsuarioDTO(
-
         Long id,
 
         @NotBlank(message = "Informe seu nome")
@@ -13,6 +12,14 @@ public record UsuarioDTO(
 
         @NotBlank(message = "Informe uma senha")
         @Size(min = 4, max = 60, message = "A senha deve ter entre 4 e 60 caracteres")
-        String senha
+        String senha,
 
-) { }
+        @NotBlank(message = "Informe o nome do primeiro animal de estimação")
+        String primeiroAnimal,
+
+        @NotBlank(message = "Informe a cidade onde nasceu")
+        String cidadeNascimento,
+
+        @NotBlank(message = "Informe o apelido da infância")
+        String apelidoInfancia
+) {}
